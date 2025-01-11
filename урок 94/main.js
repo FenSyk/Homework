08a71d87add8d705859895d0f37e3bdfa45795ff
FenSyk <138034115+FenSyk@users.noreply.github.com>
2025-01-11 16:55:44 +0300

    Add files via upload

---
+++ урок 94/main.js
@@ -0,0 +1,12 @@
+// Объявляем переменные с описаниями
+let namePerson = "Дмитрий"; 
+let agePerson = 29;
+let cityPerson = "Сыктывкар";
+let professionPerson = "Программист";
+let hobbyPerson = "Игры";
+
+console.log("Имя: " + namePerson);
+console.log("Возраст: " + agePerson);
+console.log("Город: " + cityPerson);
+console.log("Профессия: " + professionPerson);
+console.log("Хобби: " + hobbyPerson);
