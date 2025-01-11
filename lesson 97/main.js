96a0fc6bb22893c07ec0837ebc6f0dfebb4fe80e
FenSyk <138034115+FenSyk@users.noreply.github.com>
2025-01-11 17:06:29 +0300

    Add files via upload

---
+++ lesson 97/main.js
@@ -0,0 +1,9 @@
+let age = 30;
+if (age >= 18) {
+    console.log("Вы совершеннолетний");
+} else {
+    console.log("Вам еще нет 18");
+}
+
+let message = (age >= 18) ? "Вы совершеннолетний" : "Вам еще нет 18";
+console.log(message);
