5a27a1cdf211ce06cdc0dc0b527d9b67ea8ed223
FenSyk <138034115+FenSyk@users.noreply.github.com>
2025-01-12 22:20:16 +0300

    Add files via upload

---
+++ lesson 99/main.js
@@ -0,0 +1,25 @@
+for (let i = 2; i <= 12; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+
+let i = 2;
+while (i <= 12) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+
+let j = 10;
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
