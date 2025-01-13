aeee37d2c4d25662d628e1f7668b07daa3095e4b
FenSyk <138034115+FenSyk@users.noreply.github.com>
2025-01-13 21:09:30 +0300

    Update main.js

--- lesson 112/main.js
+++ lesson 112/main.js
@@ -1,23 +1,17 @@
-// Получаем элементы
-// Получаем элементы
 const btnOpen = document.querySelector('.btn-open');
 const modal = document.querySelector('.modal');
 const modalCloseBtn = document.querySelector('.modal__close-btn');
 
-// Функция для открытия модального окна
 btnOpen.addEventListener('click', () => {
     modal.classList.add('active');
 });
 
-// Функция для закрытия модального окна
 function closeModal() {
     modal.classList.remove('active');
 }
 
-// Закрытие модалки по кнопке
 modalCloseBtn.addEventListener('click', closeModal);
 
-// Закрытие модалки по клику на подложку
 modal.addEventListener('click', (event) => {
     if (event.target === modal) {
         closeModal();
